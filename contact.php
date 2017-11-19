033a519a3ded805f9d385001f8732884695f335e
Noah Cardoza <noahcardoza@gmail.com>
2017-11-18 19:22:38 -0800

    now saves post data to a file since the php server is limited and I am lazy

--- contact.php
+++ contact.php
@@ -1,3 +1,41 @@
+<?
+
+date_default_timezone_set('America/Los_Angeles');
+
+function ip()
+{
+	if (isset($_SERVER['HTTP_CLIENT_IP']))
+		return $_SERVER['HTTP_CLIENT_IP'];
+	else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+		return$_SERVER['HTTP_X_FORWARDED_FOR'];
+	else if(isset($_SERVER['HTTP_X_FORWARDED']))
+		return$_SERVER['HTTP_X_FORWARDED'];
+	else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+		return$_SERVER['HTTP_FORWARDED_FOR'];
+	else if(isset($_SERVER['HTTP_FORWARDED']))
+		return$_SERVER['HTTP_FORWARDED'];
+	else if(isset($_SERVER['REMOTE_ADDR']))
+		return$_SERVER['REMOTE_ADDR'];
+	else
+		return'UNKNOWN';
+}
+
+if (isset($_POST['message']) || isset($_POST['email'])){
+	$endl = "\n";
+	$date = date('Y-H-i-s');
+	$contents =  $date . $endl;
+	$contents .= 'IP : ' . ip() . $endl;
+	$contents .= 'FROM : ' . (isset($_POST['email']) ? $_POST['email'] : 'UNKNOWN') . $endl; 
+	$contents .= "--- START MESSAGE ---" . $endl;
+	$contents .= (isset($_POST['message']) ? $_POST['message'] : 'NONE') . $endl;
+	if (!is_dir('messages')){
+		mkdir('messages', 0777);
+	}
+	file_put_contents('messages/' . $date . '.msg', $contents);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" data-nav="contact">
 <head>
@@ -9,7 +47,27 @@
 <body>
 	<div class="wrapper">
 		<?include("navbar.html");?>
-		<section class="spacer-lg"><h1>Gmail Is The New Email</h1></section>
+		<section class="spacer-lg"><h1>
+			<?
+				if (isset($_POST['message']) || isset($_POST['email'])){
+					$endl = "\n";
+					$date = date('Y-H-i-s');
+					$contents =  $date . $endl;
+					$contents .= 'IP : ' . ip() . $endl;
+					$contents .= 'FROM : ' . (isset($_POST['email']) ? $_POST['email'] : 'UNKNOWN') . $endl; 
+					$contents .= "--- START MESSAGE ---" . $endl;
+					$contents .= (isset($_POST['message']) ? $_POST['message'] : 'NONE') . $endl;
+					$contents .= (isset($_POST['message']) ? $_POST['message'] : 'NONE') . $endl;
+					if (!is_dir('messages')){
+						mkdir('messages', 0777);
+					}
+					file_put_contents('./messages/' . $date . '.msg', $contents);
+					echo("Thanks For Your Input!");
+				} else {
+					echo("Gmail Is The New Email");
+				}
+			?>	
+		</h1></section>
 		<div class="divider"><span>I say all publicity is good publicity, except spam!</span></div>
 		<section class="main">
 			<article>
@@ -17,7 +75,7 @@
 				<sub>If you have any comments, concerns, or death threats, those go here.</sub>
 				<form id="contact-form" method="POST">
 					<input type="email" name="email" placeholder="Email (optional)">
-					<textarea spellcheck="false" name="msg" class="msg-input" placeholder="Type your heart out!"></textarea>
+					<textarea spellcheck="false" name="message" class="msg-input" placeholder="Type your heart out!"></textarea>
 					<input type="submit" value="Submit">
 				</form>
 			</article>
