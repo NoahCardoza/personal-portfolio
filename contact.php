a965dd2c7a1e95260f1bb8aeff444b78a97af863
Noah Cardoza <noahcardoza@gmail.com>
2017-11-18 19:46:56 -0800

    server won't let me write to a file....

--- contact.php
+++ contact.php
@@ -1,23 +1,23 @@
 <?
-date_default_timezone_set('America/Los_Angeles');
+// date_default_timezone_set('America/Los_Angeles');
 
-function ip()
-{
-	if (isset($_SERVER['HTTP_CLIENT_IP']))
-		return $_SERVER['HTTP_CLIENT_IP'];
-	else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-		return$_SERVER['HTTP_X_FORWARDED_FOR'];
-	else if(isset($_SERVER['HTTP_X_FORWARDED']))
-		return$_SERVER['HTTP_X_FORWARDED'];
-	else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-		return$_SERVER['HTTP_FORWARDED_FOR'];
-	else if(isset($_SERVER['HTTP_FORWARDED']))
-		return$_SERVER['HTTP_FORWARDED'];
-	else if(isset($_SERVER['REMOTE_ADDR']))
-		return$_SERVER['REMOTE_ADDR'];
-	else
-		return'UNKNOWN';
-}
+// function ip()
+// {
+// 	if (isset($_SERVER['HTTP_CLIENT_IP']))
+// 		return $_SERVER['HTTP_CLIENT_IP'];
+// 	else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+// 		return$_SERVER['HTTP_X_FORWARDED_FOR'];
+// 	else if(isset($_SERVER['HTTP_X_FORWARDED']))
+// 		return$_SERVER['HTTP_X_FORWARDED'];
+// 	else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+// 		return$_SERVER['HTTP_FORWARDED_FOR'];
+// 	else if(isset($_SERVER['HTTP_FORWARDED']))
+// 		return$_SERVER['HTTP_FORWARDED'];
+// 	else if(isset($_SERVER['REMOTE_ADDR']))
+// 		return$_SERVER['REMOTE_ADDR'];
+// 	else
+// 		return'UNKNOWN';
+// }
 ?>
 
 <!DOCTYPE html>
@@ -34,17 +34,17 @@ function ip()
 		<section class="spacer-lg"><h1>
 			<?
 				if (isset($_POST['message']) && isset($_POST['email'])){
-					$endl = "\n";
-					$date = date('Y-H-i-s');
-					$contents =  $date . $endl;
-					$contents .= 'IP : ' . ip() . $endl;
-					$contents .= 'FROM : ' . ($_POST['email'] ? $_POST['email'] : 'UNKNOWN') . $endl; 
-					$contents .= "--- START MESSAGE ---" . $endl;
-					$contents .= ($_POST['message'] ? $_POST['message'] : 'NONE') . $endl;
-					if (!is_dir('./messages')){
-						mkdir('./messages', 0777);
-					}
-					var_dump(file_put_contents('./messages/' . $date . '.msg', $contents));
+					// $endl = "\n";
+					// $date = date('Y-H-i-s');
+					// $contents =  $date . $endl;
+					// $contents .= 'IP : ' . ip() . $endl;
+					// $contents .= 'FROM : ' . ($_POST['email'] ? $_POST['email'] : 'UNKNOWN') . $endl; 
+					// $contents .= "--- START MESSAGE ---" . $endl;
+					// $contents .= ($_POST['message'] ? $_POST['message'] : 'NONE') . $endl;
+					// if (!is_dir('./messages')){
+					// 	mkdir('./messages', 0777);
+					// }
+					// var_dump(file_put_contents('./messages/' . $date . '.msg', $contents));
 					echo("Thanks For Your Input!");
 				} else {
 					echo("Gmail Is The New Email");
