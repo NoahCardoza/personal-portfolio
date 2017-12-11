7bf14857b2c3ac2b318e04ae18edc3b2edd1f070
Noah Cardoza <noahcardoza@gmail.com>
2017-12-10 23:43:13 -0800

    changed link from Google search to https://goo.gl/Muk2Rd

--- about.php
+++ about.php
@@ -22,7 +22,7 @@
             <sub>Noah Cardoza - Attends <a target="_blank" href="https://www.deanza.edu/">De Anza</a></sub>
           </div>
           <!-- ARTICLE BODY -->
-          <p>To sum it up in just a few words, I'm a 17 year-old computer enthusiast. I'm just finishing up my first quarter at <a target="_blank" href="https://www.deanza.edu/">De Anza</a> community college. A pretty great quarter over all. I am also enrolled at <a target="_blank" href="http://42.us.org">42</a>, which is a tuition free programming university funded by French billionaire <a target="_blank" href="https://www.google.com/search?q=Xavier+Niel">Xavier Niel</a>. How cool is that?</p>
+          <p>To sum it up in just a few words, I'm a 17 year-old computer enthusiast. I'm just finishing up my first quarter at <a target="_blank" href="https://www.deanza.edu/">De Anza</a> community college. A pretty great quarter over all. I am also enrolled at <a target="_blank" href="http://42.us.org">42</a>, which is a tuition free programming university funded by French billionaire <a target="_blank" href="https://goo.gl/Muk2Rd">Xavier Niel</a>. How cool is that?</p>
           <p>I spend my free time messing around with my Mac Book Pro and starting all kinds of wacky projects.</p>
           <p>Stay tuned for more!</p>
         </article>
